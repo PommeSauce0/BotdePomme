c5142206e1427d22d12e76cee0f5135351b8de48
Louis <90215125+PommeSauce0@users.noreply.github.com>
2023-03-02 14:26:09 +0100

    update games.py (list in config)

--- commands/games.py
+++ commands/games.py
@@ -9,13 +9,16 @@ class Games(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print("Games is online")
+        print("Les commandes de jeux sont connectées.")
 
     # Jeu du pendu
     @commands.command(name='pendu', help='Jouer au pendu')
     async def pendu(self, ctx):
         # Liste de mots pour le pendu
-        mots = ['chat', 'chien', 'voiture', 'ordinateur', 'maison']
+        # mots = ['chat', 'chien', 'voiture', 'ordinateur', 'maison']
+        liste_mot = open("./config/liste_pendu.txt", "r")
+        mots = liste_mot.read().split(',')
+        # print(mots)
         mot_a_deviner = random.choice(mots)
         lettres_trouvees = []
         nombre_de_coups = 0
@@ -26,34 +29,37 @@ class Games(commands.Cog):
 
         while nombre_de_coups < max_coups:
             lettre = (await self.bot.wait_for('message', check=lambda message: message.author == ctx.author)).content
-            if lettre in lettres_trouvees:
-                await ctx.send("Vous avez déjà trouvé cette lettre.")
-            elif lettre in mot_a_deviner:
-                lettres_trouvees.append(lettre)
-                mot_masque = ''.join([lettre if lettre in lettres_trouvees else '-' for lettre in mot_a_deviner])
-                await ctx.send(f"Bravo ! La lettre {lettre} est dans le mot. Mot à deviner : {mot_masque}")
-                if '-' not in mot_masque:
-                    await ctx.send(
-                        f"Félicitations ! Vous avez deviné le mot {mot_a_deviner} en {nombre_de_coups + 1} coups.")
-                    return
+            if lettre == mot_a_deviner:
+                await ctx.send(f"Bien joué, le mot à trouvé était bien {mot_a_deviner}")
+                return
             else:
-                nombre_de_coups += 1
-                await ctx.send(
-                    f"Désolé, la lettre {lettre} n'est pas dans le mot. Il vous reste {max_coups - nombre_de_coups} coups.")
-                if nombre_de_coups == max_coups:
-                    await ctx.send(f"Désolé, vous avez épuisé tous vos coups. Le mot était {mot_a_deviner}.")
+                if lettre in lettres_trouvees:
+                    await ctx.send("Vous avez déjà trouvé cette lettre.")
+                elif lettre in mot_a_deviner:
+                    lettres_trouvees.append(lettre)
+                    mot_masque = ''.join([lettre if lettre in lettres_trouvees else '-' for lettre in mot_a_deviner])
+                    await ctx.send(f"Bravo ! La lettre {lettre} est dans le mot. Mot à deviner : {mot_masque}")
+                    if '-' not in mot_masque:
+                        await ctx.send(
+                            f"Félicitations ! Vous avez deviné le mot {mot_a_deviner} en {nombre_de_coups + 1} coups.")
+                        return
+                else:
+                    nombre_de_coups += 1
+                    await ctx.send(
+                        f"Désolé, la lettre {lettre} n'est pas dans le mot. Il vous reste {max_coups - nombre_de_coups} coups.")
+                    if nombre_de_coups == max_coups:
+                        await ctx.send(f"Désolé, vous avez épuisé tous vos coups. Le mot était {mot_a_deviner}.")
 
-    @commands.command(name='Pile ou Face', help='Pile ou Face quoi')
+    @commands.command(name='pof', help='Pile ou Face quoi')
     async def pof(self, ctx):
-        cpof = ["heads", "tails", "the coin did not fall :D you owe me 2€"]
-        await ctx.send("*throws the coin in the air*")
+        cpof = ["face", "pile", "La pièce n'est pas tomber... Tu me dois 2€"]
+        await ctx.send("*Jette la pièce en l'air*")
         await asyncio.sleep(3)
-        await ctx.send("*Noise of a piece spinning in the air because I have too much force*")
+        await ctx.send("*Bruit d'une pièce qui tourne*")
         await asyncio.sleep(2)
-
         dpof = random.choice(cpof)
-        if not dpof == "the coin did not fall :D you owe me 2€":
-            await ctx.send("It's {}".format(dpof))
+        if not dpof == "La pièce n'est pas tomber... Tu me dois 2€":
+            await ctx.send("C'est {}".format(dpof))
         else:
             await ctx.send(dpof)
 
